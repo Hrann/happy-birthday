b672826fa9be18ce2237d6b20bf743ab6f57450c
Huiran Song <husong@ucsd.edu>
2021-01-17 20:53:44 -0800

    customized and added music

--- script/main.js
+++ script/main.js
@@ -189,14 +189,14 @@ const animationTimeline = () => {
         scale: 3.5,
         opacity: 0,
         x: 25,
-        y: -25,
+        y: -20,
         rotationZ: -45
       },
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
+      x: -110,
+      y: 330,
       rotation: -180,
       opacity: 0
     })
